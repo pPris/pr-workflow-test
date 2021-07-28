b3865717a22f2960cb01e1662ca72f90b744ad58
priscilla <49683462+pPris@users.noreply.github.com>
2021-07-29 01:09:01 +0800

    Handling newly opened pr (#2)
    * Add comment and label to newly opened PR

--- .github/pr_opened/main.js
+++ .github/pr_opened/main.js
@@ -6,15 +6,21 @@ async function run() {
         const token = core.getInput("repo-token");
         const octokit = github.getOctokit(token);
 
+        core.info("Octokit has been set up");
+
         // params to set
-        const _owner = github.context.owner;
-        const _repo = github.context.repo;
+        // check https://github.com/actions/toolkit/blob/main/packages/github/src/context.ts to figure out what's being responded
+        const _owner = github.context.repo.owner; 
+        const _repo = github.context.repo.repo;
         const _actor = github.context.actor;
-        const commentBody = `Hi ${_actor}, Thank you for contributing. Please comment \`@bot ready for review\` when you're ready to request a review.`
-        core.info(github.context);
+        const commentBody = `Hi ${_actor}, thank you for contributing :tada: Please comment \`@bot ready for review\` when you're ready to request a review.`
+        core.info(github.context.issue);
 
         const _issue_num = github.context.issue.number;
-
+        console.log(github.context.issue);
+        console.log(_issue_num);
+        console.log(_owner)
+        console.log(_repo)
 
         const comment = await octokit.rest.issues.createComment({
             owner: _owner,
@@ -23,15 +29,21 @@ async function run() {
             issue_number: _issue_num
         })
 
+        core.info("Commented: " + commentBody);
+
         const label = await octokit.rest.issues.addLabels({
             owner: _owner,
             repo: _repo,
-            body: commentBody,
             issue_number: _issue_num,
-            labels: {name: "S.Ongoing"}
+            labels: ["S.Ongoing"]
         })
 
+        core.info("label has been added");
+
     } catch (ex) {
+        core.info(ex);
         core.setFailed(ex.message);
     }
 }
+
+run();
