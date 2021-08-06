25660d2e566bb512b989993e389c6d8d8157d280
ppris <49683462+pPris@users.noreply.github.com>
2021-08-06 19:32:45 +0800

    fix

--- .github/assign_reviewer/main.ts
+++ .github/assign_reviewer/main.ts
@@ -9,10 +9,14 @@ const repo = github.context.repo.repo;
 // const issue_number = github.context.issue.number; // underscore to follow the api 
 // const ref = github.context.ref;
 
+//* to change in production
+
 // topic : array of people who can be assigned 
 const possibleAssignees = {
     any: ["ppris"]
 }
+
+const hoursBefAutoAssign = 6;
 
 async function run() {
     try {
@@ -45,22 +49,16 @@ async function getOpenPRs() {
 
         // should i check if checks are still passing and add a precautionary message
 
-        const diff = (Date.now() - Date.parse(pr.updated_at)) / (1000 * 60 * 60 * 24);
+        // const diff = (Date.now() - Date.parse(pr.updated_at)) / (1000 * 60 * 60 * 24);
         
-        //// https://octokit.github.io/rest.js/v18#issues-list-events
-        const events = await octokit.rest.issues.listEvents({
-            owner,
-            repo,
-            issue_number,
-          })
-          .then(res => {core.info(JSON.stringify(res))}) 
-          .catch(err => {throw err})
+
+        const tooSoon = await wasToReviewLabelAddedInTheLast(24, pr)
         
         // todo note that scope was to assign 24 hours after label..
-        if (diff < 1) {
-            core.info("PR had activity in the last 24 hours, skipping as a precaution..."); // actually even commits count as activity so you need to check if it's contributor activity...
-            continue;
-        } 
+        // if (diff < 1) {
+        //     core.info("PR had activity in the last 24 hours, skipping as a precaution..."); // actually even commits count as activity so you need to check if it's contributor activity...
+        //     continue;
+        // } 
 
         const assignees = pickAssignees();
 
@@ -83,7 +81,44 @@ function pickAssignees() {
     // todo return possibleAssignees.any 
 }
 
+// function displayDebugInfo
+
 run();
+
+/**
+ * Returns true if the to review label was added only in the last 24 hours. 
+ * Assumes that the label is on this PR.
+ * @param hours 
+ */
+async function wasToReviewLabelAddedInTheLast(hours : number, pr) : Promise<boolean> {
+    core.info(`checking if label was added in the last ${hours} hours...`)
+    const issue_number = pr.number;
+
+    // get an array of events for the current issue
+    //// https://octokit.github.io/rest.js/v18#issues-list-events
+    const events = await octokit.rest.issues.listEvents({
+        owner,
+        repo,
+        issue_number,
+        })
+        .then(res => res.data) 
+        .catch(err => {throw err})
+
+    
+    const labelEvent = events.find(e => e.event === "labeled" && e.label?.name == "s.ToReview") ;
+
+    if (!labelEvent) {
+        core.info("label event was not found on this pr");
+        return false;
+    }
+
+    const diffInHours = (Date.now() - Date.parse(labelEvent!.created_at)) / (1000 * 60 * 60);
+
+    const res = diffInHours < hours;
+
+    core.info(`difference is ${diffInHours} hours. is it less than ${hours}: ${res}`);
+    return res;
+}
 
 /*
 check what the issue update events is returning
