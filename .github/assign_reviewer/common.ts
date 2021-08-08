d25756b013f43132110ff8ebd4b222166eb894ba
priscilla <49683462+pPris@users.noreply.github.com>
2021-08-08 19:18:57 +0800

    Assign reviewer fix (#9)
    * Write logic for when PR needs to be assigned reviewer
    * Modularising code is in progress
    * Merging so that workflow runs with updated action code

---
+++ .github/assign_reviewer/common.ts
@@ -0,0 +1,34 @@
+/**
+ * Collection of common functions to use in this folder
+ */
+import core = require("@actions/core");
+import github = require("@actions/github");
+const token = core.getInput("repo-token");
+const octokit = github.getOctokit(token);
+
+const owner = github.context.repo.owner;
+const repo = github.context.repo.repo;
+// todo move one level up
+
+export async function wereReviewCommentsAdded(pr, sinceTimeStamp : string) {
+    isValidTimestamp(sinceTimeStamp);
+
+    const comments = await octokit.rest.pulls.listReviewComments({
+        owner,
+        repo,
+        pull_number: pr.number,
+        since: sinceTimeStamp // todo unsure if this works as expected --> test
+      })
+      .then(res => {core.info("these comments were retrieved\n" + res); return res;}) // todo log properly w pr number
+      .catch(err => {throw err});
+    
+    return comments.data.length > 0;
+}   
+
+function isValidTimestamp(sinceTimeStamp: string) {
+    try {
+        Date.parse(sinceTimeStamp);
+    } catch (err) {
+        throw new Error(`the sinceTimeStamp argument passed is an invalid timestamp`);
+    }
+}
--- .github/assign_reviewer/main.ts
+++ .github/assign_reviewer/main.ts
@@ -1,33 +1,19 @@
 import core = require("@actions/core");
 import github = require("@actions/github");
-const token = core.getInput("repo-token");
-const octokit = github.getOctokit(token);
+import { relabelPRsAfterReview as relabelPRsAfterReview } from "./changeToReviewToOngoing";
+import { findPRsAndAssignReviewers } from "./assignReviewersToPRs";
 
-const owner = github.context.repo.owner;
-const repo = github.context.repo.repo;
-const actor = github.context.actor;
-const issueNum = github.context.issue.number;
-const ref = github.context.ref;
+// todo rename this folder to periodic workflows, and most of the info here should be related to assign reviewer --> move it
 
 async function run() {
     try {
-        await getOpenPRs();
+        await findPRsAndAssignReviewers();
+        await relabelPRsAfterReview(); // should move to own file
+        core.info("workflow ran successfully");
     } catch (ex) {
         core.info(ex);
         core.setFailed(ex.message);
     }
-}
-
-async function getOpenPRs() {
-    await octokit.rest.issues.listForRepo({
-        owner,
-        repo,
-        state: "open",
-        labels: "s.Ongoing",
-        assignee: "none"
-    })
-    .then(res => core.info(JSON.stringify(res)))
-    .catch(err => { core.info(err); throw err })
 }
 
 run();
