9834a71440585506a26ff53f45ccb53b35d6a57e
ppris <49683462+pPris@users.noreply.github.com>
2021-08-08 18:20:28 +0800

    Edits and change PR ref for rfr

--- .github/assign_reviewer/main.ts
+++ .github/assign_reviewer/main.ts
@@ -1,6 +1,6 @@
 import core = require("@actions/core");
 import github = require("@actions/github");
-import { changeLabelsAfterReview } from "./changeToReviewToOngoing";
+import { relabelPRsAfterReview as relabelPRsAfterReview } from "./changeToReviewToOngoing";
 import { findPRsAndAssignReviewers } from "./assignReviewersToPRs";
 
 // todo rename this folder to periodic workflows, and most of the info here should be related to assign reviewer --> move it
@@ -8,7 +8,8 @@ import { findPRsAndAssignReviewers } from "./assignReviewersToPRs";
 async function run() {
     try {
         await findPRsAndAssignReviewers();
-        await changeLabelsAfterReview();
+        await relabelPRsAfterReview(); // should move to own file
+        core.info("workflow ran successfully");
     } catch (ex) {
         core.info(ex);
         core.setFailed(ex.message);
